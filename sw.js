700f221790e24c5a0a2d26539c296f3f99f4914b
santiago <nunezdugar@gmail.com>
2026-04-09 17:26:00 -0300

    app v-1

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'finanzas-v3.3-cache'; // ACTUALIZACIÓN TEMAS Y CORAL
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Fuerza a la nueva versión a activarse de inmediato
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
